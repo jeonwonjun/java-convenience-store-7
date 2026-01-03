778616724b14a54615530a1ccc8bd7b7a3aacb96
전원준 <jwjjwj1960@naver.com>
2026-01-03 16:08:53 +0900

    feat: File, Select Enum 저장
    File: filePath를 저장한 객체
    Select: 프로모션, 멤버십에 대한 사용자 선택에 대한 객체

--- src/main/java/store/model/File.java
+++ src/main/java/store/model/File.java
@@ -1,4 +1,21 @@
 package store.model;
 
+import java.util.List;
+
 public enum File {
+    PRODUCTS("./src/main/resources/products.md", ),
+    PROMOTIONS("./src/main/resources/promotions.md", );
+
+    private final String path;
+    File(String path) {
+        this.path = path;
+    }
+
+    public static List<File> getAllFile() {
+        return List.of(File.values());
+    }
+
+    public String getPath() {
+        return path;
+    }
 }
--- src/main/java/store/model/Select.java
+++ src/main/java/store/model/Select.java
@@ -1,4 +1,22 @@
 package store.model;
 
-public class Select {
+import java.util.Arrays;
+import store.util.ErrorMessage;
+
+public enum Select {
+    YES("Y"),
+    NO("N");
+
+    private final String description;
+
+    Select(String description) {
+        this.description = description;
+    }
+
+    public static Select findSelect(String description) {
+        return Arrays.stream(Select.values())
+                .filter(o -> o.description.equals(description))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_INPUT_FORMAT.getMessage()));
+    }
 }
