bc9a2ee28d7b1c1ee2ed4390f03823e1185c58e9
전원준 <jwjjwj1960@naver.com>
2026-01-03 14:19:57 +0900

    feat: FileScanner 추가
    File을 읽어드릴 수 있는 공통적인 함수 생성

--- src/main/java/store/util/FileScanner.java
+++ src/main/java/store/util/FileScanner.java
@@ -1,4 +1,28 @@
 package store.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileScanner {
+    private FileScanner() {
+    }
+
+    public static List<String> readFile(String filePath) {
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            List<String> fileBody = new ArrayList<>();
+            while (scanner.hasNext()) {
+                fileBody.add(scanner.next());
+            }
+            return fileBody;
+        } catch (IOException e) {
+            throw new IllegalStateException(ErrorMessage.INVALID_FILE_READ.getMessage());
+        }
+    }
 }
