b294228e45077f3fcab7173ace05aa030af15c19
전원준 <jwjjwj1960@naver.com>
2026-01-03 14:18:54 +0900

    feat: ErrorMessage 추가
    파일 리더 오류
    수량, 형식 오류
    존재하지 않는 상품 예외
    구매 재고 수량 초과 예외
    기타 잘못된 입력 예외

---
+++ src/main/java/store/util/ErrorMessage.java
@@ -0,0 +1,21 @@
+package store.util;
+
+public enum ErrorMessage {
+    INVALID_FILE_READ("파일을 읽어올 수 없습니다."),
+    INVALID_PRODUCT_QUANTITY_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    INVALID_PRODUCT_EXITS("존재하지 않는 상품입니다. 다시 입력해 주세요."),
+    INVALID_PRODUCT_QUANTITY("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
+    INVALID_INPUT_FORMAT("잘못된 입력입니다. 다시 입력해 주세요.");
+
+
+    private final String message;
+    private static final String ERROR_FORMAT = "[ERROR]";
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_FORMAT + " " + message;
+    }
+}
