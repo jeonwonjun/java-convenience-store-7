1035e42bf0ed69e05412518db922213a19da0cdc
전원준 <jwjjwj1960@naver.com>
2026-01-04 17:42:21 +0900

    feat: 주문을 담는 OrderRequestDto 생성

--- src/main/java/store/dto/OrderRequest.java
+++ src/main/java/store/dto/OrderRequest.java
@@ -1,4 +1,19 @@
 package store.dto;
 
 public class OrderRequest {
+    private final String productName;
+    private final int quantity;
+
+    public OrderRequest(String productName, int quantity) {
+        this.productName = productName;
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
